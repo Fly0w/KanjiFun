0ccd860cd3771211ea45bb0d7d73f7827d614935
dionysius <dionysius.politis@raketech.com>
2023-12-23 03:05:26 +0900

    Integrating vite into ASP.Net

--- Program.cs
+++ Program.cs
@@ -1,22 +1,36 @@
-// <snippet_HubsNamespace>
 using SignalRWebpack.Hubs;
-// </snippet_HubsNamespace>
+using ViteDotNet;
+using ViteDotNet.Middleware;
 
-// <snippet_AddSignalR>
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddViteIntegration(builder.Configuration); //Allow Vite.NET to read your configuration.
 builder.Services.AddSignalR();
-// </snippet_AddSignalR>
 
-// <snippet_FilesMiddleware>
 var app = builder.Build();
 
 app.UseDefaultFiles();
-app.UseStaticFiles();
-// </snippet_FilesMiddleware>
 
-// <snippet_MapHub>
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.RunViteDevServer("./ClientApp"); //Middleware for running your Vite app alongside your ASP.NET Core app.
 app.MapHub<ChatHub>("/hub");
-// </snippet_MapHub>
 
 app.Run();
